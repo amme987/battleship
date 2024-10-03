2c75020827907d1ce0043f7f967f3da37308f4cd
amme987 <emmagination97@gmail.com>
2024-10-03 11:27:51 -0400

    Build gameboard and placeShip()

--- script.js
+++ script.js
@@ -4,9 +4,11 @@ export class Ship {
     this.hits = 0;
   }
 
+  // Increases the number of 'hits' in your ship
   hit() {
     return this.hits++;
   }
+
   isSunk() {
     if (this.hits === this.length) {
       return true;
@@ -14,4 +16,28 @@ export class Ship {
       return false;
     }
   }
+}
+
+export class Gameboard {
+  constructor() {
+    this.gameboard = Array.from({ length: 10 }, () => Array(10).fill([]));
+  }
+
+  placeShip(coordinates, ship) {
+    if (coordinates[0] + ship.length <= 10) {
+      ship.coordinates = [];
+      for (let i = 0; i < ship.length; i++) {
+        ship.coordinates.push([coordinates[0] + i, coordinates[1]]);
+      }
+    } else if (coordinates[1] + ship.length <= 10) {
+      ship.coordinates = [];
+      for (let i = 0; i < ship.length; i++) {
+        ship.coordinates.push([coordinates[0], coordinates[1] + i]);
+      }
+    }
+  }
+
+  // Takes a pair of coordinates, determines whether or not the attack hit a ship and then sends the ‘hit’ function to
+  // the correct ship, or records the coordinates of the missed shot.
+  receiveAttack() {}
 }
--- script.test.js
+++ script.test.js
@@ -1,15 +1,45 @@
-import { Ship } from './script';
+import { Ship, Gameboard } from './script';
 
 let ship = new Ship(3);
+let board = new Gameboard();
 
-test('hits', () => {
-  ship.hit();
-  expect(ship.hits).toBe(1);
+describe('Ship', () => {
+  test('hits', () => {
+    ship.hit();
+    expect(ship.hits).toBe(1);
+  });
+
+  test('is sunk', () => {
+    ship.hit();
+    expect(ship.isSunk()).toBe(false);
+    ship.hit();
+    expect(ship.isSunk()).toBe(true);
+  });
 });
 
-test('is sunk', () => {
-  ship.hit();
-  expect(ship.isSunk()).toBe(false);
-  ship.hit();
-  expect(ship.isSunk()).toBe(true);
+describe('Gameboard', () => {
+  test('create board', () => {
+    expect(board.gameboard.length).toBe(10);
+    board.gameboard.forEach(row => expect(row.length).toBe(10));
+  });
+
+  test('place ship horizontally', () => {
+    board.placeShip([0, 5], ship);
+    console.log(ship.coordinates);
+    expect(ship.coordinates).toStrictEqual([
+      [0, 5],
+      [1, 5],
+      [2, 5],
+    ]);
+  });
+
+  test('place ship vertically', () => {
+    board.placeShip([10, 0], ship);
+    console.log(ship.coordinates);
+    expect(ship.coordinates).toStrictEqual([
+      [10, 0],
+      [10, 1],
+      [10, 2],
+    ]);
+  });
 });
