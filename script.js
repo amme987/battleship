8b321195408ceabdabf5f7e64fd3716542ed4229
amme987 <emmagination97@gmail.com>
2024-10-09 14:17:15 -0400

    Add input for player name

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
-import { Ship, Gameboard, Player } from './objects';
+import { Ship, Gameboard, Player } from './objects.js';
 
-let player = new Player('Sally');
+let form = document.querySelector('form');
+form.addEventListener('submit', e => {
+  e.preventDefault();
+  const name = document.getElementById('player').value;
+  displayBoard(name);
+});
+
+function displayBoard(name) {
+  const player = new Player(name);
+}
